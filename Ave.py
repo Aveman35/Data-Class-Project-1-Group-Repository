0d616784fc9ef499ea70f962680f488c9a960b48
Avery Javier <saveryj35@gmail.com>
2024-10-31 20:21:16 -0700

    Updated with project outline

---
+++ Ave.py
@@ -0,0 +1,14 @@
+#What we are planning to do wiht the data 
+
+#Source - Dashboard: https://data.oregon.gov/stories/s/ODF-Fire-History-2000-2022-Dashboard/92y3-mdk3
+#Source - CSV and JSON: https://catalog.data.gov/dataset/odf-fire-occurrence-data-2000-2022
+
+#Focus parameters:
+
+#1. Analysis on Number of fires for 2012-2022 (Human vs lightning)
+#2. Anaylsis on Total Acres Burned for 2012 - 2022 (Human vs lightning)
+#3. Analysis on General Cause of fires for 2012 - 2022
+#4. Analysis on Districts  with the most fires (Top 10 or bigger if it makes sense)
+#5. Analysis on number of fires per month per year 2012 - 2022 (June-October)
+
+#Timeline for project
