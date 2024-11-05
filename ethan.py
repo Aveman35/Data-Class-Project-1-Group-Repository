4103291d20a50fa9d17ec5e347241e968479e18a
ethanshipman <124001912+ethanshipman@users.noreply.github.com>
2024-11-04 20:00:11 -0700

    updated my file with decision and intentions

---
+++ ethan.py
@@ -0,0 +1,8 @@
+#NOTES
+
+#Oregon Fire Data covering 22 years of reported fires
+
+# General Cause of Fires
+# - aggregate every cause
+# - analyze the amounts caused by each
+# - Viszualize using bar, pie, and (maybe) Scatter plot
