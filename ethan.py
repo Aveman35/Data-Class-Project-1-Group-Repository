c9b3e45f426c77fb29e88a85e348bd2d5801a0c7
ethanshipman <124001912+ethanshipman@users.noreply.github.com>
2024-11-07 21:54:54 -0700

    Delete ethan.py

--- ethan.py
+++
@@ -1,8 +0,0 @@
-#NOTES
-
-#Oregon Fire Data covering 22 years of reported fires
-
-# General Cause of Fires
-# - aggregate every cause - sort by years
-# - analyze the amounts caused by each per year
-# - Viszualize using bar, pie, and Line plot
